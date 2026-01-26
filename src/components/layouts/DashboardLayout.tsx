69f5036d6083b65b72aa7189e296d6907ae595cc
Kevin <kevinhernandezcrespo97@gmail.com>
2026-01-25 23:36:52 -0500

    feat: Add `sonner` package and refactor `DashboardLayout` to include a styled `Sidebar`, `header`, and `main` content area.

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -17,13 +17,45 @@ const styles = {
       "sidebar content"
     `,
   },
+  sidebar: {
+    gridArea: "sidebar",
+    overflowY: "auto",
+    height: "100%",
+    width: "200px",
+    position: "sticky",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 100,
+  },
+  header: {
+    gridArea: "header",
+    padding: "1rem",
+    backgroundColor: "#fff",
+    borderBottom: "1px solid #ccc",
+  },
+  content: {
+    gridArea: "content",
+    padding: "1rem",
+    overflowY: "auto",
+    height: "100%",
+    width: "100%",
+    position: "sticky",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: 100,
+  },
 } satisfies Record<string, CSSProperties>;
 
 const DashboardLayout: FC<Props> = ({ style, ...rest }) => {
   return (
     <div {...rest} style={{ ...styles.container, ...style }}>
-      <Sidebar />
-      <Outlet />
+      <Sidebar style={styles.sidebar} />
+      <header style={styles.header}></header>
+      <main style={styles.content}>
+        <Outlet />
+      </main>
     </div>
   );
 };
--- src/components/widgets/Sidebar.tsx
+++ src/components/widgets/Sidebar.tsx
@@ -1,5 +1,9 @@
-const Sidebar = () => {
-  return <div>sidebar</div>;
+import type { FC } from "react";
+
+interface SidebarProps extends React.ComponentProps<"aside"> {}
+
+const Sidebar: FC<SidebarProps> = ({ style, ...rest }) => {
+  return <aside {...rest} style={{ ...style }}></aside>;
 };
 
 export default Sidebar;
